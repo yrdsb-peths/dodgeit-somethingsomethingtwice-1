3f35eeafe4d241e87cbc90f065bd7e71b0fe729e
somethingsomethingtwice <167238930+somethingsomethingtwice@users.noreply.github.com>
2024-09-12 12:53:46 -0400

    Hero movement

--- Hero.java
+++ Hero.java
@@ -8,12 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Hero extends Actor
 {
-    /**
-     * Act - do whatever the Hero wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    boolean atTop = true;
+    
     public void act()
     {
-        // Add your action code here.
+        if (Greenfoot.mousePressed(null))
+            atTop = !atTop;
+        
+        if (atTop)
+            setLocation(100, 100);
+        else
+            setLocation(100, 300);
     }
 }
---
+++ Strawberry.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Collectible object
+ * 
+ * @author Ethan
+ * @version 12 September, 2024
+ */
+public class Strawberry extends Actor
+{
+    /**
+     * Act - do whatever the Strawberry wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
